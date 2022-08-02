2a9b5f70f74292e85edbd5c8bf20e4438e6e6904
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2022-08-02 18:21:40 +0900

    Update InquiryVo.java

--- src/main/java/com/lunchwb/vo/InquiryVo.java
+++ src/main/java/com/lunchwb/vo/InquiryVo.java
@@ -9,14 +9,155 @@ public class InquiryVo {
 	private String inquiryFilePath;
 	private String inquiryDate;
 	private String inquiryStatus;
-	
-	
+
 	//////////////////// replyInquiry table ////////////////////
 	private int replyNo;
 	// private int inquiryNo;
 	private int adminNo;
 	private String replyContent;
+
+	// 생성자
+	public InquiryVo() {
+	};
 	
+	
+	//회원이 문의등록 시
+	// 파일 X
+	public InquiryVo(int userNo, String inquiryTitle, String inquiryContent) {
+		this.userNo = userNo;
+		this.inquiryTitle = inquiryTitle;
+		this.inquiryContent = inquiryContent;
+	};
+	
+	// 파일 O
+	public InquiryVo(int userNo, String inquiryTitle, String inquiryContent, String inquiryFilePath) {
+		this.userNo = userNo;
+		this.inquiryTitle = inquiryTitle;
+		this.inquiryContent = inquiryContent;
+		this.inquiryFilePath = inquiryFilePath;
+	};
 	
 
-}
+
+	public InquiryVo(int inquiryNo, int userNo, String inquiryTitle, String inquiryContent, String inquiryFilePath,
+			String inquiryDate, String inquiryStatus) {
+		this.inquiryNo = inquiryNo;
+		this.userNo = userNo;
+		this.inquiryTitle = inquiryTitle;
+		this.inquiryContent = inquiryContent;
+		this.inquiryFilePath = inquiryFilePath;
+		this.inquiryDate = inquiryDate;
+		this.inquiryStatus = inquiryStatus;
+	};
+
+	public InquiryVo(int replyNo, int inquiryNo, int adminNo, String replyContent) {
+		this.replyNo = replyNo;
+		this.inquiryNo = inquiryNo;
+		this.adminNo = adminNo;
+		this.replyContent = replyContent;
+	};
+
+	public InquiryVo(int inquiryNo, int userNo, String inquiryTitle, String inquiryContent, String inquiryFilePath,
+			String inquiryDate, String inquiryStatus, int replyNo, int adminNo, String replyContent) {
+		this.inquiryNo = inquiryNo;
+		this.userNo = userNo;
+		this.inquiryTitle = inquiryTitle;
+		this.inquiryContent = inquiryContent;
+		this.inquiryFilePath = inquiryFilePath;
+		this.inquiryDate = inquiryDate;
+		this.inquiryStatus = inquiryStatus;
+		this.replyNo = replyNo;
+		this.adminNo = adminNo;
+		this.replyContent = replyContent;
+	};
+
+	// 메소드 - gs
+	public int getInquiryNo() {
+		return inquiryNo;
+	};
+
+	public void setInquiryNo(int inquiryNo) {
+		this.inquiryNo = inquiryNo;
+	};
+
+	public int getUserNo() {
+		return userNo;
+	};
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	};
+
+	public String getInquiryTitle() {
+		return inquiryTitle;
+	};
+
+	public void setInquiryTitle(String inquiryTitle) {
+		this.inquiryTitle = inquiryTitle;
+	};
+
+	public String getInquiryContent() {
+		return inquiryContent;
+	};
+
+	public void setInquiryContent(String inquiryContent) {
+		this.inquiryContent = inquiryContent;
+	};
+
+	public String getInquiryFilePath() {
+		return inquiryFilePath;
+	};
+
+	public void setInquiryFilePath(String inquiryFilePath) {
+		this.inquiryFilePath = inquiryFilePath;
+	};
+
+	public String getInquiryDate() {
+		return inquiryDate;
+	};
+
+	public void setInquiryDate(String inquiryDate) {
+		this.inquiryDate = inquiryDate;
+	};
+
+	public String getInquiryStatus() {
+		return inquiryStatus;
+	};
+
+	public void setInquiryStatus(String inquiryStatus) {
+		this.inquiryStatus = inquiryStatus;
+	};
+
+	public int getReplyNo() {
+		return replyNo;
+	};
+
+	public void setReplyNo(int replyNo) {
+		this.replyNo = replyNo;
+	};
+
+	public int getAdminNo() {
+		return adminNo;
+	};
+
+	public void setAdminNo(int adminNo) {
+		this.adminNo = adminNo;
+	};
+
+	public String getReplyContent() {
+		return replyContent;
+	};
+
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	};
+
+	@Override
+	public String toString() {
+		return "InquiryVo [inquiryNo=" + inquiryNo + ", userNo=" + userNo + ", inquiryTitle=" + inquiryTitle
+				+ ", inquiryContent=" + inquiryContent + ", inquiryFilePath=" + inquiryFilePath + ", inquiryDate="
+				+ inquiryDate + ", inquiryStatus=" + inquiryStatus + ", replyNo=" + replyNo + ", adminNo=" + adminNo
+				+ ", replyContent=" + replyContent + "]";
+	};
+
+};
