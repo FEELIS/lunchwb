2216c8f79b44f2d7ac9d2897ea04e42a1905e84f
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2022-08-05 18:07:38 +0900

    통계 컨트롤러만

---
+++ src/main/java/com/lunchwb/controller/StatController.java
@@ -0,0 +1,79 @@
+package com.lunchwb.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/stat")
+public class StatController {
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	
+	// ============================================ 캘린더 ============================================
+	// -------------------- 달력폼 --------------------
+	@GetMapping("/statCalendar")
+	public String statCalendarForm() {
+		logger.info("StatController > statCalendarForm()");
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		return "stat/statCalendar";
+	};
+	
+	
+	
+	
+	// ============================================ 리뷰 ============================================
+	// -------------------- 리뷰내역폼 --------------------
+	@GetMapping("/reviewList")
+	public String reviewListForm() {
+		logger.info("StatController > reviewListForm()");
+		
+		
+		
+		
+		
+		
+		return "stat/reviewList";
+	};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	// ============================================ 통계 ============================================
+	// -------------------- 통계폼 --------------------
+	@GetMapping("/statChart")
+	public String statChartForm() {
+		logger.info("StatController > statChartForm()");
+		
+		
+		
+		
+		
+		return "stat/statChart";
+	};
+	
+	
+	
+	
+}
+
