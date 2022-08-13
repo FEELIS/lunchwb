8c34c4c923ede1ef084eb220fc821fa44c498c5f
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2022-08-13 23:02:22 +0900

    Update AloneDao.java

--- src/main/java/com/lunchwb/dao/AloneDao.java
+++ src/main/java/com/lunchwb/dao/AloneDao.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ReviewDao {
+public class AloneDao {
 	
 	@Autowired
 	private SqlSession sqlSession;
