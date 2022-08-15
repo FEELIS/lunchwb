84875f043eb7a0d88f660dcac226fc6c631695ee
Junho Kim <chordplaylist@gmail.com>
2022-08-15 13:29:22 +0900

    22_08_15 비밀번호 찾기, pom.xml email 의존성 추가

--- src/main/java/com/lunchwb/service/UserService.java
+++ src/main/java/com/lunchwb/service/UserService.java
@@ -5,19 +5,22 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.mail.HtmlEmail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
 import com.lunchwb.dao.UserDao;
 import com.lunchwb.vo.UserVo;
 
@@ -261,5 +264,80 @@ public class UserService {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	/* 비밀번호 */
+	public void sendEmail(UserVo userVo, String div) throws Exception {
+		// Mail Server 설정
+		String charSet = "utf-8";
+		String hostSMTP = "smtp.gmail.com"; //네이버 이용시 smtp.naver.com
+		String hostSMTPid = "lunchwb@gmail.com";
+		String hostSMTPpwd = "qnwkdsladyrldy1!";
+
+		// 보내는 사람 EMail, 제목, 내용
+		String fromEmail = "lunchwb@gmail.com";
+		String fromName = "부장님요기요";
+		String subject = "";
+		String msg = "";
+
+		if(div.equals("findpw")) {
+			subject = "부장님요기요 임시 비밀번호 입니다.";
+			msg += "<div align='center' style='border:1px solid black; font-family:verdana'>";
+			msg += "<h3 style='color: blue;'>";
+			msg += userVo.getUserEmail() + "님의 임시 비밀번호 입니다. 비밀번호를 변경하여 사용하세요.</h3>";
+			msg += "<p>임시 비밀번호 : ";
+			msg += userVo.getUserPassword() + "</p></div>";
+		}
+
+		// 받는 사람 E-Mail 주소
+		String mail = userVo.getUserEmail();
+		try {
+			HtmlEmail email = new HtmlEmail();
+			email.setDebug(true);
+			email.setCharset(charSet);
+			email.setSSL(true);
+			email.setHostName(hostSMTP);
+			email.setSmtpPort(465); //네이버 이용시 587
+
+			email.setAuthentication(hostSMTPid, hostSMTPpwd);
+			email.setTLS(true);
+			email.addTo(mail, charSet);
+			email.setFrom(fromEmail, fromName, charSet);
+			email.setSubject(subject);
+			email.setHtmlMsg(msg);
+			email.send();
+		} catch (Exception e) {
+			System.out.println("메일발송 실패 : " + e);
+		}
+	}
+
+	/*
+	//비밀번호찾기
+	public void findPw(HttpServletResponse response, UserVo userVo) throws Exception {
+		response.setContentType("text/html;charset=utf-8");
+		UserVo ck = userDao.readMember(userVo.getUserEmail());
+		PrintWriter out = response.getWriter();
+		
+		// 가입된 이메일이 아니면
+		if(!userVo.getUserEmail().equals(ck.getUserEmail())) {
+			out.print("등록되지 않은 이메일입니다.");
+			out.close();
+		}else {
+			// 임시 비밀번호 생성
+			String pw = "";
+			for (int i = 0; i < 12; i++) {
+				pw += (char) ((Math.random() * 26) + 97);
+			}
+			userVo.setPw(pw);
+			// 비밀번호 변경
+			userDao.updatePw(userVo);
+			// 비밀번호 변경 메일 발송
+			sendEmail(userVo, "findpw");
+
+			out.print("이메일로 임시 비밀번호를 발송하였습니다.");
+			out.close();
+		}
+	}
+	*/
 
 }
