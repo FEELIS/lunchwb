de45741976ce6e2eb09798102108405d9ba538b8
gardenee <snnjjang@naver.com>
2022-08-18 20:08:51 +0900

    0818 voteVo 생성자, getter setter

--- src/main/java/com/lunchwb/vo/VoteVo.java
+++ src/main/java/com/lunchwb/vo/VoteVo.java
@@ -10,11 +10,97 @@ public class VoteVo {
 	private String voteItems;
 	private String voteResults;
 	
+	
 	//////////////////// vote_members table ////////////////////
 	private int voteMemberNo;
 	// private int voteNo; (vote table fk)
 	private int groupMemberNo;
 	private String userName;
+	
+	
+	////////// 생성자 ////////////////////////////////////////
+	
+	public VoteVo() {
+		
+	}
+
+	
+	/////////// getter setter ////////////////////////////////
+
+	public int getVoteNo() {
+		return voteNo;
+	}
+
+	public void setVoteNo(int voteNo) {
+		this.voteNo = voteNo;
+	}
+
+	public int getVoteMadeUser() {
+		return voteMadeUser;
+	}
+
+	public void setVoteMadeUser(int voteMadeUser) {
+		this.voteMadeUser = voteMadeUser;
+	}
+
+	public int getGroupNo() {
+		return groupNo;
+	}
+
+	public void setGroupNo(int groupNo) {
+		this.groupNo = groupNo;
+	}
+
+	public String getVoteEndTime() {
+		return voteEndTime;
+	}
+
+	public void setVoteEndTime(String voteEndTime) {
+		this.voteEndTime = voteEndTime;
+	}
+
+	public String getVoteItems() {
+		return voteItems;
+	}
+
+	public void setVoteItems(String voteItems) {
+		this.voteItems = voteItems;
+	}
+
+	public String getVoteResults() {
+		return voteResults;
+	}
+
+	public void setVoteResults(String voteResults) {
+		this.voteResults = voteResults;
+	}
+
+	public int getVoteMemberNo() {
+		return voteMemberNo;
+	}
+
+	public void setVoteMemberNo(int voteMemberNo) {
+		this.voteMemberNo = voteMemberNo;
+	}
+
+	public int getGroupMemberNo() {
+		return groupMemberNo;
+	}
+
+	public void setGroupMemberNo(int groupMemberNo) {
+		this.groupMemberNo = groupMemberNo;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+		
+	///// toString //////////////////////////////////////////
 	
 
 }
