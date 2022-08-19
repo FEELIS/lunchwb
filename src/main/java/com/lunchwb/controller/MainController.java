2875fdfc6159ac7fa3d7bb59f04b22c8ea43080b
gardenee <snnjjang@naver.com>
2022-08-19 13:54:34 +0900

    0819 403, 404, 500 jsp 생성

--- src/main/java/com/lunchwb/controller/MainController.java
+++ src/main/java/com/lunchwb/controller/MainController.java
@@ -1,9 +1,15 @@
 package com.lunchwb.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lunchwb.vo.UserVo;
 
 
 @Controller
@@ -12,11 +18,19 @@ public class MainController {
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
 	/* ----- 메인 접근 --------------------------------------------*/
-	@GetMapping("/")
-	public String Main() {
+	@RequestMapping("/")
+	public String main(@PathVariable(required = false) Integer voteNo, HttpSession session) {
 		logger.info("main");
 		
+		UserVo loginUser = (UserVo)session.getAttribute("authUser");
+		
+		
 		return "main/index";
+	}
+	
+	@RequestMapping("/404")
+	public String error() {
+		return "error/404";
 	}
 	
 }
