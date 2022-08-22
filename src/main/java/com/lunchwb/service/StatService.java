a7c5ae4c541ffb3f7861f5bd7a4d311478f6b914
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2022-08-22 14:17:36 +0900

    220822 통계

--- src/main/java/com/lunchwb/service/StatService.java
+++ src/main/java/com/lunchwb/service/StatService.java
@@ -22,20 +22,27 @@ public class StatService {
 	private static final Logger logger = LoggerFactory.getLogger(StatService.class);
 	
 	// 지난 한달간 음식별 여기갈래요 수 조회
-	public Map<String,List<String>> getStatChart(String menu1stCateName){
+	public Map<String,List<String>> getStatChart(StatVo statVo){
 		List<String> menu1stCateNameList = new ArrayList<String>();
-		menu1stCateNameList.add(menu1stCateName);
+		menu1stCateNameList.add(statVo.getMenu1stCateName());
+		
 		
 		
 		// 지난 한달간 음식별 여기갈래요 수 조회
 		List<StatVo> lastMonthList = statDao.lastMonth();
+		// 로그인 한 유저의 그룹간 식사수
+		List<StatVo> withGroupList = statDao.withGroup(statVo.getUserNo());
 		// 지난 6개월 간 선택한 음식의 인기 조회
-		List<StatVo> getFoodHitList = statDao.getFoodHit(menu1stCateName);
+		List<StatVo> getFoodHitList = statDao.getFoodHit(statVo.getMenu1stCateName());
 		// 지난 6개월 간 선택한 음식의 인기 조회
-		List<StatVo> getAgeHitList = statDao.getAgeHit(menu1stCateName);
+		List<StatVo> getAgeHitList = statDao.getAgeHit(statVo.getMenu1stCateName());
+		
 		
 		List<String> barLabels = new ArrayList<String>();
 		List<String> barData = new ArrayList<String>();
+		
+		List<String> doughnutLabels = new ArrayList<String>();
+		List<String> doughnutData = new ArrayList<String>();
 		
 		List<String> lineLabels = new ArrayList<String>();
 		List<String> lineData = new ArrayList<String>();
@@ -46,33 +53,42 @@ public class StatService {
 		Map<String,List<String>> lmMap = new HashMap<String,List<String>>();
 		
 		// JSON.parse() 위해 "\"" 양끝 추가
+		// 최근 한 달 음식별 인기
 		for (int i = 0; i < lastMonthList.size(); i++) {
 			barLabels.add("\""+lastMonthList.get(i).getMenu1stCateName()+"\"");
 			barData.add("\""+lastMonthList.get(i).getVisitCount()+"\"");
-			
 		}
 		
+		// 그룹간 식사횟수
+		for (int i = 0; i < withGroupList.size(); i++) {
+			doughnutLabels.add("\""+withGroupList.get(i).getGroupName()+"\"");
+			doughnutData.add("\""+withGroupList.get(i).getVisitCount()+"\"");
+		}
+		
+		// 선택한 음식 6개월간 인기
 		for (int i = 0; i < getFoodHitList.size(); i++) {
 			if (Integer.parseInt(getFoodHitList.get(i).getVisitedDate())<10) {
 				lineLabels.add("\""+getFoodHitList.get(i).getVisitedDate().substring(1)+"월\"");
-				
 			} else {
 				lineLabels.add("\""+getFoodHitList.get(i).getVisitedDate()+"월\"");
-
 			}
 			lineData.add("\""+getFoodHitList.get(i).getVisitCount()+"\"");
-			
 		}
+		
+		// 연령별 인기
 		for (int i = 0; i < getAgeHitList.size(); i++) {
 			pieLabels.add("\""+getAgeHitList.get(i).getAgeGroup()+"\"");
 			logger.info(getAgeHitList.get(i).getAgeGroup());
 			pieData.add("\""+getAgeHitList.get(i).getVisitCount()+"\"");
-			
 		}
+		
+		
 		
 		lmMap.put("menu1stCateName", menu1stCateNameList);
 		lmMap.put("barLabels", barLabels);
 		lmMap.put("barData", barData);
+		lmMap.put("doughnutLabels", doughnutLabels);
+		lmMap.put("doughnutData", doughnutData);
 		lmMap.put("lineLabels", lineLabels);
 		lmMap.put("lineData", lineData);
 		lmMap.put("pieLabels", pieLabels);
