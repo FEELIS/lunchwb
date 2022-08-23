1f3643becc9fb8a05a3cdfff31f82ddf0f1b49fd
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2022-08-23 19:15:36 +0900

    문의 파일업로드 추가

--- src/main/java/com/lunchwb/service/InquiryService.java
+++ src/main/java/com/lunchwb/service/InquiryService.java
@@ -1,9 +1,15 @@
 package com.lunchwb.service;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.lunchwb.dao.InquiryDao;
 import com.lunchwb.vo.InquiryVo;
@@ -21,8 +27,57 @@ public class InquiryService {
 		return inqList;
 	};
 	
-	public int writeInquiry(InquiryVo inqVo) {
-		int count = inquiryDao.writeInquiry(inqVo);
+	public int writeInquiry(InquiryVo inqVo,MultipartFile file) {
+		
+		int count = 0;
+		
+		String saveDir = "/Users/choijungphil/javaStudy/upload";
+		String orgName = "";
+		String saveName = "";
+
+		if (file.getOriginalFilename().equals("")) {	//블로그 타이틀만 바꿀 경우 방지
+			System.out.println("이미 미업로드시");
+			inqVo.setInquiryFilePath(saveName);
+
+			// (1)다오로 보내서 DB 업데이트
+			count = inquiryDao.writeInquiry(inqVo);
+
+		} else {	// 파일업로드 할 때 작용
+			// 오리지널 파일명
+			orgName = file.getOriginalFilename();
+			System.out.println("orgName: " + orgName);
+
+			// 확장자명 가져오기
+			String exName = orgName.substring(orgName.lastIndexOf("."));
+
+			// 드라이브에 저장할 파일명
+			saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exName;
+			System.out.println("saveName: " + saveName);
+
+			// 파일경로(디렉토리+저장파일명)
+			String filePath = saveDir + "/" + saveName;
+			
+			inqVo.setInquiryFilePath(saveName);
+
+			// (1)다오로 보내서 DB 업데이트
+			count = inquiryDao.writeInquiry(inqVo);
+
+			// (2)파일저장
+			try {
+				byte[] fileData = file.getBytes();
+				OutputStream os = new FileOutputStream(filePath);
+				BufferedOutputStream bos = new BufferedOutputStream(os);
+
+				bos.write(fileData);
+				bos.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
 		
 		return count;
 	};
