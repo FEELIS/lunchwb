9f4735212565d1a62472d93c6bb7b8a0d1676a91
이지희 <som2plestory@naver.com>
2022-08-30 14:53:22 +0900

    .

--- src/main/java/com/lunchwb/dao/NotiDao.java
+++ src/main/java/com/lunchwb/dao/NotiDao.java
@@ -15,14 +15,35 @@ public class NotiDao {
 	private SqlSession sqlSession;
 	
 	
+	/* 알림 개수 */
+	public int notiCount(int userNo) {
+		return sqlSession.selectOne("notification.notiCount", userNo);
+	}
+	
+	
 	/* 그룹 이름 변경 알림 */
 	public int alertOfGroupChange(NotificationVo notiVo) {
 		return sqlSession.insert("notification.alertOfGroupChange", notiVo);
 	}
 	
-	/* 개인 알림 */
-	public int addNoti(Map<String, Object> map) {
-		return sqlSession.insert("notification.addNoti", map);
+	/* 그룹원 알림 1/5/6/10 */
+	public int addGroupNoti(NotificationVo notiVo) {
+		return sqlSession.insert("notification.addGroupNoti", notiVo);
+	}
+
+	/* 그룹장 알림 2/3/4 */
+	public int addLeaderNoti(NotificationVo notiVo) {
+		return sqlSession.insert("notification.addLeaderNoti", notiVo);
+	}
+	
+	/* 리뷰 삭제 알림 8 */
+	public int addReportNoti(NotificationVo notiVo) {
+		return sqlSession.insert("notification.addReportNoti", notiVo);
+	}
+	
+	/* 문의 답변 알림 9 */
+	public int addReplyNoti(NotificationVo notiVo) {
+		return sqlSession.insert("notification.addReplyNoti", notiVo);
 	}
 	
 	/* 그룹원 리스트 : 초대중인 멤버 수 */
