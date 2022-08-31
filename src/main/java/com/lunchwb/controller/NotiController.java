c0f583366d13078797d803062702ef70b216051a
이지희 <som2plestory@naver.com>
2022-09-01 04:21:13 +0900

    그룹 초대 수락했을 때 바스켓 세션 처리

--- src/main/java/com/lunchwb/controller/NotiController.java
+++ src/main/java/com/lunchwb/controller/NotiController.java
@@ -1,6 +1,9 @@
 package com.lunchwb.controller;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.lunchwb.service.BasketService;
 import com.lunchwb.service.NotiService;
+import com.lunchwb.vo.GPSVo;
+import com.lunchwb.vo.GroupVo;
 import com.lunchwb.vo.NotificationVo;
+import com.lunchwb.vo.StoreVo;
 
 @RequestMapping("/notice")
 @Controller
@@ -20,6 +27,8 @@ public class NotiController {
 	
 	@Autowired
 	private NotiService notiService;
+	@Autowired
+	private BasketService basketService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(NotiController.class);
 	
@@ -51,10 +60,48 @@ public class NotiController {
 	/*************************** 그룹 초대 수락 *******************************************/
 	@ResponseBody
 	@PostMapping("/group/invite/ok")
-	public String inviteOk(@RequestBody NotificationVo notiVo) {
-		logger.info("inviteOk...notiVo={}", notiVo);
+	public String inviteOk(@RequestBody NotificationVo notiVo, HttpSession session) {
+		logger.info("inviteOk...notiVo={}", notiVo);	
 		
 		String result = notiService.inviteOk(notiVo);
+		
+		//basket
+		if(result == "success") {
+			List<GroupVo> basketGroup = (List<GroupVo>)session.getAttribute("basketGroup");
+			if(basketGroup != null) {
+				
+				GroupVo groupVo = new GroupVo();
+				groupVo.setGroupNo(notiVo.getGroupNo());
+				groupVo.setGroupName(notiVo.getGroupName());
+				
+				basketGroup = basketService.basketGroupAdd(basketGroup, groupVo);
+			}
+			
+			session.setAttribute("basketGroup", basketGroup);
+			
+			int groupNo = notiVo.getGroupNo();
+			int groupCount = notiVo.getGroupOrder();
+			
+			if(groupCount > 1) {
+				
+				GPSVo curr_location = (GPSVo)session.getAttribute("curr_location");
+				List<Integer> filter_excluded = (List<Integer>)session.getAttribute("filter_excluded");
+				Map<Integer, List<StoreVo>> basket = (Map<Integer, List<StoreVo>>)session.getAttribute("basket");
+				
+				basket = basketService.addBasketGroup(basket, groupNo);
+				basket.put(groupNo, basketService.addItemsToBasket(basket.get(groupNo), groupNo, curr_location, filter_excluded, true, true));
+				
+				session.setAttribute("basket", basket);
+				
+			}else {
+				if (session.getAttribute("basket") != null) {
+					session.removeAttribute("basket");
+				}
+				session.setAttribute("curr-basket-group", groupNo);
+			}
+			
+		}
+
 		
 		return result;
 	}
